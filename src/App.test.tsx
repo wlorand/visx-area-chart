50fc509bb7e74e8c55d0f705c482b901f4779bb0
Will Lorand <will@milemarker3.org>
2020-10-27 13:13:33 -0700

    cra initial cleanup

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+
+import { render } from '@testing-library/react';
+
+// child components
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App component', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+  // TODO: define first assertion for component to be there - react-testting-lib 101
 });
