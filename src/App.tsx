dba28f3855e3ebcd68c5413b806aa2c47a0e49af
Will Lorand <will@milemarker3.org>
2020-10-28 05:31:11 -0700

    added starter component and rendered svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// child components
+import AppleStockAreaChart from './components/AppleStockAreaChart';
+
 // styles
 import './app.css';
 
@@ -7,6 +10,7 @@ function App() {
   return (
     <div className="app">
       <h1 className="app-title">Visx Area Chart</h1>
+      <AppleStockAreaChart />
     </div>
   );
 }
---
+++ src/components/AppleStockAreaChart.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AppleStockAreaChart() {
+  return <svg width={640} height={480} className="chart-svg"></svg>;
+}
+
+export default AppleStockAreaChart;
